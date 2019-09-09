60bbaed3c4948c88ceab0473922b4a862c5706f6
ny0011 <nayun011@gmail.com>
2019-09-09 12:20:13 +0900

    Updates

---
+++ precache-manifest.a5278963436b8337b7b847543649df37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b6ff3f9365e2ce619b26ec3ff11bd5",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "75fe3b4b141085bb6fc4",
+    "url": "/movie-app/static/css/main.5e73b74c.chunk.css"
+  },
+  {
+    "revision": "af5933e30f60e2789a6b",
+    "url": "/movie-app/static/js/2.55d0086c.chunk.js"
+  },
+  {
+    "revision": "75fe3b4b141085bb6fc4",
+    "url": "/movie-app/static/js/main.bc59fe5d.chunk.js"
+  },
+  {
+    "revision": "28e0d62ff273c9689a71",
+    "url": "/movie-app/static/js/runtime~main.37b6632d.js"
+  }
+]);
